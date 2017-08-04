590139114808e1663da06197aa86f82c3d342c17
杨剑彬 <yangjianbin@yangjianbindeMacBook-Pro.local>
2017-08-04 16:50:09 +0800

    dd

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo going to stop self-service
-PROCESS=`ps -ef|grep self-service | awk '{ print $2}'`
+PROCESS=`ps -ef|grep self-service | grep java | awk '{ print $2}'`
 for i in $PROCESS
 do
   echo "Kill the $1 process [ $i ]"
